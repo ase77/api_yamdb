6ededf78ee98acfb53a4a80ac2252e4f54fd2328
Моторин Антон <ust.mir@gmail.com>
2022-05-15 23:22:50 +0300

    Edit: admin, Fixed: merge conflicts

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,54 @@
+from django.contrib import admin
+
+from .models import User, Category, Genre, Title, Review, Comment
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk', 'username', 'email', 'first_name', 'last_name', 'role'
+    )
+    search_fields = ('username',)
+    list_filter = ('role',)
+    list_editable = ('role',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    search_fields = ('slug',)
+    list_filter = ('slug',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    search_fields = ('slug',)
+    list_filter = ('slug',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'year', 'rating', 'genre', 'category')
+    search_fields = ('name',)
+    list_filter = ('genre', 'category')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'author', 'text', 'title', 'pub_date', 'score')
+    search_fields = ('author', 'title')
+    list_filter = ('author', 'title')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'author', 'review', 'text', 'pub_date')
+    search_fields = ('author', 'review')
+    list_filter = ('author', 'review')
+    empty_value_display = '-пусто-'
